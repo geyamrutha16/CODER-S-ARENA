37ace08c3b4244fe1544507ee1aab4d1bd7fe8aa
Geyamrutha Poluru <shinchanharry148@gmail.com>
2024-08-24 07:13:31 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Nav from './components/Home/Nav';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Calendar from './components/Schedules/Schedule';
+import Resources from './components/Resources/Resources';
+import Timeline from './components/Events/Timeline';
+import Loader from './components/Home/Loaderlogo';
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a delay to represent data fetching
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000); // 2 seconds delay
+
+    // Clean up the timer when the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/events" element={<Timeline />} />
+            <Route path="/resources" element={<Resources />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </>
+      )}
+    </Router>
   );
-}
+};
 
 export default App;
